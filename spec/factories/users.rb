ba85621ee19802315cf9ed494a2fca6a26a7d94e
Nestor Castillo <nestor.castillo@izea.com>
2015-04-05 15:38:54 -0400

    Adding user test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:email) { |n| "user#{n}@example.com" }
+    password 'top_secret'
+    password_confirmation 'top_secret'
+    confirmed_at { Time.now }
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 describe User, type: :model do
-  pending "Put tests in here"
+  it 'is valid with an email and password' do
+    user = build(:user)
+
+    expect(user).to be_valid
+  end
+
+  it 'is invalid without an email' do
+    user = build(:user, email: nil)
+
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid without a password' do
+    user = build(:user, password: nil, password_confirmation: nil)
+
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalide if passwords do not match' do
+    user = build(:user, password: nil, password_confirmation: 'gibberish124')
+
+    expect(user).to_not be_valid
+  end
 end
