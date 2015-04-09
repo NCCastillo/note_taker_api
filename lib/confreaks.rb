693109b5f16403b40b35f48c6ceaa77102ff316c
Nestor Castillo <nestor.castillo@izea.com>
2015-04-08 21:24:03 -0400

    Refactored to class methods to represent the api end points and added event_count/events

--- lib/confreaks.rb
+++ lib/confreaks.rb
@@ -3,13 +3,38 @@ require 'json'
 
 module Confreaks
   class Client
-    def self.get(api_options)
-      if api_options[:attribute].blank?
-        response = HTTParty.get("http://confreaks.tv/api/v1/#{api_options[:route]}.json")
+    attr_reader :conference, :conferences, :event_count, :events
+
+    def initialize(results)
+      @conferences = results[:conferences]
+      @conference = results[:conference]
+      @event_count = results[:event_count]
+      @events = results[:events]
+    end
+
+    def self.conferences(options={})
+      if options[:name]
+        response = HTTParty.get("http://confreaks.tv/api/v1/conferences/#{options[:name].dasherize}.json")
+        results = JSON.parse(response.body)
+        attributes = { conference: results }
       else
-        response = HTTParty.get("http://confreaks.tv/api/v1/#{api_options[:route]}/#{api_options[:attribute].dasherize}.json")
+        response = HTTParty.get('http://confreaks.tv/api/v1/conferences.json')
+        results = JSON.parse(response.body)
+        attributes = { conferences: results }
       end
-      JSON.parse(response.body)
+      new(attributes)
+    end
+
+    def self.event_count
+      response = HTTParty.get('http://confreaks.tv/api/v1/event_count.json')
+      attributes = { event_count: JSON.parse(response.body) }
+      new(attributes)
+    end
+
+    def self.events
+      response = HTTParty.get('http://confreaks.tv/api/v1/events.json')
+      attributes = { events: JSON.parse(response.body) }
+      new(attributes)
     end
   end
 end
--- spec/lib/confreak_client_spec.rb
+++ spec/lib/confreak_client_spec.rb
@@ -2,21 +2,44 @@ require 'rails_helper'
 require './lib/confreaks'
 
 describe Confreaks::Client do
-  it 'fetches all conferences' do
-    stub_request(:get, 'http://confreaks.tv/api/v1/conferences.json').
-    to_return(body: File.new('spec/fixtures/conferences.json', 'rb').read, status: 200)
+  context "conferences" do
+    it 'returns all conferences' do
+      stub_request(:get, 'http://confreaks.tv/api/v1/conferences.json').
+      to_return(body: File.new('spec/fixtures/conferences.json', 'rb').read, status: 200)
 
-    results = Confreaks::Client.get(route: :conferences)
+      confreaks = Confreaks::Client.conferences
 
-    expect(results.count).to eq 120
+      expect(confreaks).to be_an_instance_of Confreaks::Client
+      expect(confreaks.conferences.count).to eq 120
+    end
+
+    it 'returns one conference by name' do
+      stub_request(:get, 'http://confreaks.tv/api/v1/conferences/ruby-conference.json').
+      to_return(body: File.new('spec/fixtures/ruby-conference.json', 'rb').read, status: 200)
+
+      confreaks = Confreaks::Client.conferences(name: "ruby_conference")
+
+      expect(confreaks.conference["name"]).to eq "Ruby Conference"
+    end
   end
 
-  it 'fetches one conference' do
-    stub_request(:get, 'http://confreaks.tv/api/v1/conferences/ruby-conference.json').
-    to_return(body: File.new('spec/fixtures/ruby-conference.json', 'rb').read, status: 200)
+  context "events" do
+    it 'returns the event count' do
+      stub_request(:get, 'http://confreaks.tv/api/v1/event_count.json').
+      to_return(body: File.new('spec/fixtures/event_count.json', 'rb').read, status: 200)
 
-    results = Confreaks::Client.get(route: :conferences, attribute: "ruby_conference")
+      confreaks = Confreaks::Client.event_count
 
-    expect(results["name"]).to eq "Ruby Conference"
+      expect(confreaks.event_count["event_count"]).to eq 243
+    end
+
+    it 'returns all events' do
+      stub_request(:get, 'http://confreaks.tv/api/v1/events.json').
+      to_return(body: File.new('spec/fixtures/events.json', 'rb').read, status: 200)
+
+      confreaks = Confreaks::Client.events
+
+      expect(confreaks.events.count).to eq 243
+    end
   end
 end
