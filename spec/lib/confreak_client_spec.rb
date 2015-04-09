11f453111f439adf4696781c1e996530244158da
Nestor Castillo <nestor.castillo@izea.com>
2015-04-08 21:42:17 -0400

    refactored stub request into its own method

--- spec/lib/confreak_client_spec.rb
+++ spec/lib/confreak_client_spec.rb
@@ -2,11 +2,15 @@ require 'rails_helper'
 require './lib/confreaks'
 
 describe Confreaks::Client do
+  def fetch_from_api(file_name)
+    stub_request(:get, base_url + file_name).to_return(body: File.new('spec/fixtures/' + file_name, 'rb').read, status: 200)
+  end
+
+  let(:base_url) { 'http://confreaks.tv/api/v1/'}
+
   context "conferences" do
     it 'returns all conferences' do
-      stub_request(:get, 'http://confreaks.tv/api/v1/conferences.json').
-      to_return(body: File.new('spec/fixtures/conferences.json', 'rb').read, status: 200)
-
+      fetch_from_api('conferences.json')
       confreaks = Confreaks::Client.conferences
 
       expect(confreaks).to be_an_instance_of Confreaks::Client
@@ -14,9 +18,7 @@ describe Confreaks::Client do
     end
 
     it 'returns one conference by name' do
-      stub_request(:get, 'http://confreaks.tv/api/v1/conferences/ruby-conference.json').
-      to_return(body: File.new('spec/fixtures/ruby-conference.json', 'rb').read, status: 200)
-
+      fetch_from_api('conferences/ruby-conference.json')
       confreaks = Confreaks::Client.conferences(name: "ruby_conference")
 
       expect(confreaks.conference["name"]).to eq "Ruby Conference"
@@ -25,18 +27,14 @@ describe Confreaks::Client do
 
   context "events" do
     it 'returns the event count' do
-      stub_request(:get, 'http://confreaks.tv/api/v1/event_count.json').
-      to_return(body: File.new('spec/fixtures/event_count.json', 'rb').read, status: 200)
-
+      fetch_from_api('event_count.json')
       confreaks = Confreaks::Client.event_count
 
       expect(confreaks.event_count["event_count"]).to eq 243
     end
 
     it 'returns all events' do
-      stub_request(:get, 'http://confreaks.tv/api/v1/events.json').
-      to_return(body: File.new('spec/fixtures/events.json', 'rb').read, status: 200)
-
+      fetch_from_api('events.json')
       confreaks = Confreaks::Client.events
 
       expect(confreaks.events.count).to eq 243
