61801ad66e15cf3ec2fde8d0ee27bdddec21e2b3
Nestor Castillo <nestor.castillo@izea.com>
2015-04-11 18:31:53 -0400

    using before block instead of let.

--- spec/requests/conferences_spec.rb
+++ spec/requests/conferences_spec.rb
@@ -2,32 +2,31 @@ require 'rails_helper'
 
 describe 'Conferences API' do
 
-  let(:conference_one) { create(:conference, name: 'Ruby Conference') }
-  let(:conference_two) { create(:conference, name: 'Ember Conference') }
+  before(:each) do
+    @conference_one =  create(:conference, name: 'Ruby Conference')
+    create(:conference, name: 'Ember Conference')
+  end
 
   context 'GET /conferences' do
     it 'returns all conferences' do
-      expected_results = [{"id"=>conference_one.id, "name"=>conference_one.name}, {"id"=>conference_two.id, "name"=>conference_two.name}]
-
       get '/api/v1/conferences', {}, { 'Accept' => 'application/json' }
 
       results = JSON.parse(response.body)
 
       expect(response.status).to eq 200
-      expect(results['conferences']).to eq expected_results
+      expect(results['conferences'].count).to eq 2
     end
   end
 
   context 'GET /conferences/:id' do
     it 'returns a conference' do
-      expected_results = { "id"=>conference_one.id, "name"=>conference_one.name }
-
-      get "/api/v1/conferences/#{conference_one.id}", {}, { 'Accept' => 'application/json' }
+      get "/api/v1/conferences/#{@conference_one.id}", {}, { 'Accept' => 'application/json' }
 
       results = JSON.parse(response.body)
 
       expect(response.status).to eq 200
-      expect(results['conference']).to eq expected_results
+      expect(results['conference']['id']).to eq @conference_one.id
+      expect(results['conference']['name']).to eq @conference_one.name
     end
   end
 end
