0c05a7454cad291d022fa9a3b66a1dd3f8ea9650
Nestor Castillo <nestor.castillo@izea.com>
2015-04-19 18:46:37 -0400

    remove pending messages from tests

--- spec/models/conference_spec.rb
+++ spec/models/conference_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Conference, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Conference, type: :model do
+  pending
 end
--- spec/models/presenter_spec.rb
+++ spec/models/presenter_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Presenter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Presenter, type: :model do
+  pending
 end
